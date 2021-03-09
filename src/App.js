7a03e922ce73e051dc224336b1ba38e66cf5ea67
Darton Hemerik <73367214+dartonhemerik@users.noreply.github.com>
2021-03-08 19:32:22 -0800

    added Login page

--- src/App.js
+++ src/App.js
@@ -3,19 +3,26 @@ import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/userSlice';
+import Login from './Login';
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className='App'>
-      <header className='app'>
-        <Header />
+      <Header />
 
-        <div className="app__body">
+      {!user ? (
+        <Login />
+      ) : (
+        <div className='app__body'>
           <Sidebar />
           <Feed />
+          {/* Widgets */}
         </div>
-        {/* Widgets */}
-      </header>
+      )}
     </div>
   );
 }
